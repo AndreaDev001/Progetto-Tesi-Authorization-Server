cb77274eeca7d0a03ffab7c815c82ad5156406bf
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2023-11-21 15:35:03 +0100

    Effettuate modifiche

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded",() => {
     let usernameControl = document.getElementById("usernameControl");;
     let passwordControl = document.getElementById("passwordControl");
-    let resetButton = document.getElementById("resetButton");
 })
